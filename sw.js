526638bddc2cab7e65906ada40f975097a012b68
sophiecheyrou <cheyrousophie@gmail.com>
2026-09-24 17:51:59 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='pause5-v24e-20260924-mobile-logos';
-const CORE=['./','./index.html','./app.js','./manifest.webmanifest','./icon-192.png','./icon-512.png','./logo-academie-paris.jpg','./logo-college-actif.png','./hero-classroom.jpg','./hero-banner.jpg'];
+const CACHE='pause5-v24g-20260924-about-logo';
+const CORE=['./','./index.html','./app.js','./manifest.webmanifest','./icon-192.png','./icon-512.png','./logo-academie-paris.jpg','./logo-college-actif.png','./logo-pause5-about.png','./hero-classroom.jpg','./hero-banner.jpg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
